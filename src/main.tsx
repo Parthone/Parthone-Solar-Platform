1ffff1b2e14b20fb69fed39c52ce0baabd28acd2
Parthone <anantlab9@gmail.com>
2026-08-24 16:48:53 +0530

    Route client accounts to tenant portal

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import ClientPortal from './ClientPortal'
 import './styles.css'
+
+const path = window.location.pathname
+const Root = path.startsWith('/client') ? ClientPortal : App
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
 )
